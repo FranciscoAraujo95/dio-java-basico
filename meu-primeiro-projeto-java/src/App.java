051796bbcbc9b51392108e4d95cb0267ae7e8f26
FranciscoAraujo95 <fhydglan@gmail.com>
2024-06-12 10:58:37 -0300

    Created a new class called Pessoa

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello, World!");
+
+        Pessoa p = new Pessoa("", "", 0);
+        System.out.println("Informe o seu nome: ");
+        p.setNome(scanner.nextLine());
+        System.out.println("Informe a sua idade: ");
+        p.setIdade(scanner.nextInt());
+        System.out.println("Olá! Meu nome é " + p.getNome() + " e tenho " + p.getIdade() + " anos!!" );
+        
     }
 }
